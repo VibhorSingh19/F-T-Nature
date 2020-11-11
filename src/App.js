7a6440a61a42e9e99def6b0c6f99599e22c3cd6b
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-11 23:42:37 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 //import navbar from './Components/navbar/navbar';
 function App() {
   return (
-    <div className="App">
-      hello
+    <div >
+      <Navbar/>
     </div>
   );
 }
