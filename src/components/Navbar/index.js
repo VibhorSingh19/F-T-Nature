49c703325b2a8d7047b74c0d6560e8f62bce2957
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-12 23:18:52 +0530

    Navs

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,25 @@ import React from 'react'
 const Navbar= () => {
     return (
         <div>
-            <h1>Navvv</h1>
+            <Nav
+  activeKey="/home"
+  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
         </div>
     )
 }
