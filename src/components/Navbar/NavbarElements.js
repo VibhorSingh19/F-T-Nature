c13806c92f4d35d7db19468009e62dc440ba8d1c
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-13 23:13:16 +0530

    Updated

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -16,6 +16,7 @@ const NavBar= () => {
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
       
     </Nav>
