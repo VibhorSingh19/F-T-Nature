83db33201041b3bc3f867ddfe4726aa0c815b77c
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-13 23:12:01 +0530

    Updated

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -15,6 +15,8 @@ const NavBar= () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
       
     </Nav>
     <Nav>
