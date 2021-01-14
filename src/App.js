91e404ff393025613807181ad30d4d65a89764d1
Vibhor Singh <vibhor.formal@gmail.com>
2021-01-14 07:16:22 +0530

    Updating..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-import NavBar from './components/Navbar/NavbarElements';
-//import navbar from './Components/navbar/navbar';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 function App() {
   return (
-    <div >
-      <NavBar/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
