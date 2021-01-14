d7f5379033346952db522192608fd55c56e92bdd
Vibhor Singh <vibhor.formal@gmail.com>
2021-01-14 18:56:04 +0530

    Updates

---
+++ precache-manifest.24123eff748f199df145772314bc3144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bea3be5c8d4ff577d86c2a06cdae2a",
+    "url": "/F-T-Nature/index.html"
+  },
+  {
+    "revision": "b53707127fca6329ea61",
+    "url": "/F-T-Nature/static/css/main.712cb9e4.chunk.css"
+  },
+  {
+    "revision": "fd067e804abfe2ab13b3",
+    "url": "/F-T-Nature/static/js/2.0b9bfbc6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/F-T-Nature/static/js/2.0b9bfbc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53707127fca6329ea61",
+    "url": "/F-T-Nature/static/js/main.f77ea30d.chunk.js"
+  },
+  {
+    "revision": "37423eb6624f70687cde",
+    "url": "/F-T-Nature/static/js/runtime-main.af76f5a1.js"
+  }
+]);
